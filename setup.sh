650c87c1a8ac7d4d94c0aaaa66c577e761f4de1d
Otto van der Schaaf <oschaaf@we-amp.com>
2017-12-31 08:49:06 +0000

    Ask for token, move ci-out

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 # install dotnet
-
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
@@ -8,3 +7,7 @@ sudo apt-get install dotnet-sdk-2.0.2
 
 # install python3
 sudo apt-get install python3
+
+echo "Please give a github OAuth token for accessing the api"
+read token
+echo token > newci/.token
